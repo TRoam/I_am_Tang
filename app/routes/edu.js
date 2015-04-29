dbed301783e14da77831813c489b4ba329aef37e
tang <liulangdewoniu@gmail.com>
2015-04-29 23:34:13 +0800

    some code format

--- app/routes/edu.js
+++ app/routes/edu.js
@@ -1,11 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model:function() {
-		this.store.push('edu',{id:1,school:'tongji',startDate:"2009",endDate:'2013'});
-		this.store.push('edu',{id:2,school:'tongji',startDate:"2009",endDate:'2013'});
-		this.store.push('edu',{id:3,school:'tongji',startDate:"2009",endDate:'2013'});
-		this.store.push('edu',{id:4,school:'tongji',startDate:"2009",endDate:'2013'});
+	model: function() {
+		this.store.push('edu', {
+			id: 1,
+			school: 'tongji',
+			startDate: "2009",
+			endDate: '2013'
+		});
+		this.store.push('edu', {
+			id: 2,
+			school: 'tongji',
+			startDate: "2009",
+			endDate: '2013'
+		});
+		this.store.push('edu', {
+			id: 3,
+			school: 'tongji',
+			startDate: "2009",
+			endDate: '2013'
+		});
+		this.store.push('edu', {
+			id: 4,
+			school: 'tongji',
+			startDate: "2009",
+			endDate: '2013'
+		});
 		return this.store.all('edu');
 	}
 });
