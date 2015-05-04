9a6b6d2162c639282e01519dfd82e9d56aed8aa8
tang <liulangdewoniu@gmail.com>
2015-05-05 01:29:47 +0800

    fix a create commit bug

--- app/controllers/projects.js
+++ app/controllers/projects.js
@@ -1,5 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	
+	text:'',
+	actions:{
+		createComment:function(){
+		var comment = this.store.createRecord('comment',{
+			text:this.get('text'),
+			project:this.get('model')
+		});
+		comment.save().then(function(){
+			controller.set('text', ''); 
+			controller.get('model.comments').addObject(comment);
+		});
+	}
+	}
 });
--- app/routes/footprints.js
+++ app/routes/footprints.js
@@ -2,26 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model:function(){
-		this.store.push('footprints',{
+	 this.store.push('footprints',{
 			id:1,
-			start_at:"2009.09.01",
-			end_at  :"2013.06.30",
-			content :"Start in tongji University",
-			role    :"student"
-	});
-	this.store.push('footprints',{
+			start_at:"2013.05.01",
+			end_at  :"Now",
+			content :"SAP GRC Audit Management Team",
+			role    :"Developer"
+	 });	
+	 this.store.push('footprints',{
 			id:2,
-			start_at:"2009.09.01",
-			end_at  :"2013.06.30",
-			content :"Start in tongji University",
-			role    :"student"
+			start_at:"2012.07.01",
+			end_at  :"2013.04.30",
+			content :"SAP FIN Delivery Management",
+			role    :"intern"
 	});
 	this.store.push('footprints',{
 			id:3,
 			start_at:"2009.09.01",
 			end_at  :"2013.06.30",
-			content :"Start in tongji University",
-			role    :"student"
+			content :"TongJi University/School of Software Engineering",
+			role    :"Monitor  of   Class  3"
 	});
 	this.store.push('footprints',{
 			id:4,
@@ -33,16 +33,9 @@ export default Ember.Route.extend({
 	this.store.push('footprints',{
 			id:5,
 			start_at:"2009.09.01",
-			end_at  :"2013.06.30",
-			content :"Start in tongji University",
-			role    :"student"
-	});
-	this.store.push('footprints',{
-			id:6,
-			start_at:"2009.09.01",
-			end_at  :"2013.06.30",
-			content :"Start in tongji University",
-			role    :"student"
+			end_at  :"2012.06.30",
+			content :"Community  counselor  of  Southwest  eleven",
+			role    :"Honor of Excellent Member"
 	});
 	return this.store.all('footprints');
 	}
